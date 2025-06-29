a23f05824b654383d0c9955ef0598804da50caf9
Aravindhan <aravindhan@skills-agency.com>
2025-06-29 22:23:35 +0530

    mail changes

--- src/service/mail.service.js
+++ src/service/mail.service.js
@@ -3,36 +3,33 @@ const sendEmail = async(email, subject, emailContent) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'ecommerce1419@gmail.com',
-            pass: 'iqtyaldszzgoweap'
+            user: 'skillshackathon@gmail.com',
+            pass: 'tioq jhua yhto dxaa'
         }
     });
 
     const mailOptions = {
-        from: 'roomaton@gmail.com',
+        from: 'skillshackathon@gmail.com',
         to: email,
         subject: subject,
         html: `
             <html>
                 <body style="font-family: Arial, sans-serif; background-color: #f6f6f6; padding: 20px;">
                     <div style="max-width: 600px; margin: auto; background: #fff; border-radius: 8px; box-shadow: 0 2px 8px rgba(0,0,0,0.05); padding: 30px;">
-                        <h2 style="color: #333; margin-bottom: 20px;">Roomathon Notification</h2>
+                        <h2 style="color: #333; margin-bottom: 20px;">House Inspection</h2>
                         <div style="color: #444; font-size: 16px; line-height: 1.6;">
                             ${emailContent}
                         </div>
                         <hr style="margin: 30px 0;">
-                        <div style="font-size: 12px; color: #888;">
-                            &copy; ${new Date().getFullYear()} Roomathon. All rights reserved.
-                        </div>
                     </div>
                 </body>
             </html>
         `,
-        attachments: [
-            {filename:'HAIKU_XEANCO.COM_NONPAYER 3_Devis N°1_08-05-2025_nonpayer usecase3_Version2.pdf',
-                path:"./public/files/HAIKU_XEANCO.COM_NONPAYER 3_Devis N°1_08-05-2025_nonpayer usecase3_Version2.pdf"
-            }
-        ]
+        // attachments: [
+        //     {filename:'HAIKU_XEANCO.COM_NONPAYER 3_Devis N°1_08-05-2025_nonpayer usecase3_Version2.pdf',
+        //         path:"./public/files/HAIKU_XEANCO.COM_NONPAYER 3_Devis N°1_08-05-2025_nonpayer usecase3_Version2.pdf"
+        //     }
+        // ]
     };
 
     try {
